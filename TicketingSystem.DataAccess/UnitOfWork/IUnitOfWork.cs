37edcf9c2b937f0407788239be5702ef37c9a900
smara <smara@LAPTOP-1E2M6GME>
2025-12-23 00:29:49 +0300

    Added Unit of Work

---
+++ TicketingSystem.DataAccess/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using TicketingSystem.DataAccess.Interfaces;
+
+namespace TicketingSystem.DataAccess.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IUserRepository Users { get; }
+        ITicketRepository Tickets { get; }
+        IProductRepository Products { get; }
+        int Complete();
+    }
+}
---
+++ TicketingSystem.DataAccess/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketingSystem.DataAccess.Interfaces;
+using TicketingSystem.DataAccess.Repositories;
+
+namespace TicketingSystem.DataAccess.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly TicketingSystemDBContext _db;
+        private readonly ILoggerFactory _logger;
+
+        public IUserRepository Users { get; private set; }
+        public ITicketRepository Tickets { get; private set; }
+        public IProductRepository Products { get; private set; }
+
+        public UnitOfWork(TicketingSystemDBContext db, ILoggerFactory logger)
+        {
+            _db = db;
+            _logger = logger;
+
+            var userLogger = _logger.CreateLogger<UserRepository>();
+            var ticketLogger = _logger.CreateLogger<TicketRepository>();
+            var productLogger = _logger.CreateLogger<ProductRepository>();
+
+            Users = new UserRepository(_db, userLogger);
+            Tickets = new TicketRepository(_db, ticketLogger);
+            Products = new ProductRepository(_db, productLogger);
+
+        }
+
+        public int Complete()
+        {
+            return _db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+    }
+}
