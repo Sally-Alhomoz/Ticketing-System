b96655a0760f4a7147c42addbd0b28708cba7f90
smara <smara@LAPTOP-1E2M6GME>
2025-12-23 20:22:04 +0300

    Added Attachment repository

---
+++ TicketingSystem.DataAccess/Interfaces/IAttachmentRepository.cs
@@ -0,0 +1,10 @@
+﻿using TicketingSystem.DataAccess.Models;
+
+namespace TicketingSystem.DataAccess.Interfaces
+{
+    public interface IAttachmentRepository
+    {
+        void Add(Attachment attachment);
+        List<Attachment> GetByTicketId(Guid ticketId);
+    }
+}
---
+++ TicketingSystem.DataAccess/Repositories/AttachmentRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using TicketingSystem.DataAccess.Interfaces;
+using TicketingSystem.DataAccess.Models;
+
+namespace TicketingSystem.DataAccess.Repositories
+{
+    public class AttachmentRepository : IAttachmentRepository
+    {
+        private readonly TicketingSystemDBContext _db;
+        private readonly ILogger<AttachmentRepository> _logger;
+
+        public AttachmentRepository(TicketingSystemDBContext db, ILogger<AttachmentRepository> logger)
+        {
+            _db = db;
+            _logger = logger;
+        }
+
+        public void Add(Attachment attachment)
+        {
+            _logger.LogInformation("Adding an attachment to the database.");
+            _db.Attachments.Add(attachment);
+            _logger.LogInformation("Attachment added successfully");
+        }
+
+        public List<Attachment> GetByTicketId(Guid ticketId)
+        {
+            _logger.LogInformation("Retrieving attachments for Ticket {TicketId}", ticketId);
+            return _db.Attachments.Where(a => a.TicketId == ticketId).OrderByDescending(a => a.Id).ToList();
+        }
+    }
+}
