8018a2ba105eef62496f28f87741af84cae8c96f
smara <smara@LAPTOP-1E2M6GME>
2025-12-23 20:14:47 +0300

    added firstname and lastname

--- SharedDTOs/UserDto.cs
+++ SharedDTOs/UserDto.cs
@@ -1,16 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
-using SharedDTOs.Enum;
+﻿using SharedDTOs.Enum;
 
-namespace TicketingSystem.DataAccess.Models
+namespace SharedDTOs
 {
-    public class User
+    public class UserDto
     {
-        [Key]
         public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Username { get; set; }
-        public string Password { get; set; }
         public string Email { get; set; }
         public Role Role { get; set; }
         public UserStatus Status { get; set; }
