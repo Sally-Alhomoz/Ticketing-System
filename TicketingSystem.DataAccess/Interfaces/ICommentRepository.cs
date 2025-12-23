bafdde680ebc9769b7f1b9772e338ba5cdf50020
smara <smara@LAPTOP-1E2M6GME>
2025-12-23 20:22:31 +0300

    added comment repository

---
+++ TicketingSystem.DataAccess/Interfaces/ICommentRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using TicketingSystem.DataAccess.Models;
+
+namespace TicketingSystem.DataAccess.Interfaces
+{
+    public interface ICommentRepository
+    {
+        void Add(Comment comment);
+        List<Comment> GetCommentsByTicketId(Guid ticketId);
+        List<Comment> GetCommentsByUserId(Guid userId);
+    }
+}
---
+++ TicketingSystem.DataAccess/Repositories/CommentRepository.cs
@@ -0,0 +1,46 @@
+﻿
+
+using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using TicketingSystem.DataAccess.Interfaces;
+using TicketingSystem.DataAccess.Models;
+
+namespace TicketingSystem.DataAccess.Repositories
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly TicketingSystemDBContext _db;
+        private readonly ILogger<CommentRepository> _logger;
+
+        public CommentRepository(TicketingSystemDBContext db, ILogger<CommentRepository> logger)
+        {
+            _db = db;
+            _logger = logger;
+        }
+
+        public void Add(Comment comment)
+        {
+            _logger.LogInformation("Adding a comment to the database.");
+            _db.Comments.Add(comment);
+            _logger.LogInformation("Comment added successfully");
+        }
+
+        public List<Comment> GetCommentsByTicketId(Guid ticketId)
+        {
+            _logger.LogInformation("Fetching comments for Ticket: {TicketId}", ticketId);
+            
+            var comments = _db.Comments.Where(c => c.TicketId == ticketId).OrderBy(c => c.CreateDate).ToList();
+
+            return comments;
+        }
+
+        public List<Comment> GetCommentsByUserId(Guid userId)
+        {
+            _logger.LogInformation("Fetching comments for User {userId}", userId);
+
+            var comments = _db.Comments.Where(c => c.CreatedBy == userId).OrderBy(c => c.CreateDate).ToList();
+
+            return comments;
+        }
+    }
+}
