c6799b56e7150ae336dd60807dbae015ac2d366f
smara <smara@LAPTOP-1E2M6GME>
2025-12-23 20:15:37 +0300

    added TicketDto class

---
+++ SharedDTOs/TicketDto.cs
@@ -0,0 +1,16 @@
+﻿using SharedDTOs.Enum;
+
+namespace SharedDTOs
+{
+    public class TicketDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public TicketStatus Status { get; set; }
+        public TicketPriority Priority { get; set; }
+        public DateTime CteateDate { get; set; }
+        public int productId { get; set; }
+        public Guid CreatedBy { get; set; }
+        public Guid? AssignedTo { get; set; }
+    }
+}
