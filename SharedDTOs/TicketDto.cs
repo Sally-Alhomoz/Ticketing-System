8382529ca52acd91b10589236820dd5dd85380bb
smara <smara@LAPTOP-1E2M6GME>
2025-12-26 13:59:48 +0300

    added users full name & product name property

--- SharedDTOs/TicketDto.cs
+++ SharedDTOs/TicketDto.cs
@@ -4,13 +4,18 @@ namespace SharedDTOs
 {
     public class TicketDto
     {
+        public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public TicketStatus Status { get; set; }
         public TicketPriority Priority { get; set; }
-        public DateTime CteateDate { get; set; }
+        public DateTime CreateDate { get; set; }
         public int productId { get; set; }
         public Guid CreatedBy { get; set; }
         public Guid? AssignedTo { get; set; }
+        public string CreatedByFullName { get; set; }
+        public string AssignedToFullName { get; set; }
+        public string ProductName { get; set; }
+
     }
 }
