324de485b6bb826e2dec3e08d5be865410fac153
smara <smara@LAPTOP-1E2M6GME>
2025-12-26 17:03:12 +0300

    Added  a Ticket History Dto

---
+++ SharedDTOs/TicketHistoryDto.cs
@@ -0,0 +1,15 @@
+﻿using SharedDTOs.Enum;
+
+namespace SharedDTOs
+{
+    public class TicketHistoryDto
+    {
+        public Guid Id { get; set; }
+        public DateTime ChangeDate { get; set; }
+        public TicketStatus NewStatus { get; set; }
+        public Guid TicketId { get; set; }
+        public string TicketTitle { get; set; }
+        public Guid ChangedBy { get; set; }
+        public string ChangedyFullName { get; set; }
+    }
+}
