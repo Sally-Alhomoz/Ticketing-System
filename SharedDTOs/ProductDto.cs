69cdce9b164376007b63b60a90f7389b2aa6a5d9
smara <smara@LAPTOP-1E2M6GME>
2025-12-26 23:41:47 +0300

    added product dto

---
+++ SharedDTOs/ProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SharedDTOs
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
