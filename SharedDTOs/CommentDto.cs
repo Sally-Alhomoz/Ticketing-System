aba631140493e6db4bcf46b9c635fde2c6409e08
smara <smara@LAPTOP-1E2M6GME>
2025-12-27 01:00:54 +0300

    added comment dto

---
+++ SharedDTOs/CommentDto.cs
@@ -0,0 +1,14 @@
+﻿namespace SharedDTOs
+{
+    public class CommentDto
+    {
+        public Guid Id { get; set; }
+        public string Message { get; set; }
+
+        public DateTime CreateDate { get; set; }
+        public Guid CreatedBy { get; set; }
+        public string CreatedByFullName { get; set; }
+        public Guid TicketId { get; set; }
+        public string TicketTitle { get; set; }
+    }
+}
