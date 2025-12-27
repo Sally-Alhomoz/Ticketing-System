de3d871b910a8198008b961cb85dbffc215512e3
smara <smara@LAPTOP-1E2M6GME>
2025-12-27 13:40:24 +0300

    added user navigation property

--- TicketingSystem.DataAccess/Models/Attachment.cs
+++ TicketingSystem.DataAccess/Models/Attachment.cs
@@ -8,7 +8,9 @@ namespace TicketingSystem.DataAccess.Models
         public Guid Id { get; set; }
         public string FileName { get; set; }
         public string FilePath { get; set; }
+        public DateTime CreateDate { get; set; }
         public Guid UploadedBy { get; set; }
+        public User UploadedByUser { get; set; }
         public Guid TicketId { get; set; }
         public Guid? CommentId { get; set; }
     }
