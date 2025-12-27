c39fa9bed277fee65cbe16e2b3b1e7f54ae7a2d2
smara <smara@LAPTOP-1E2M6GME>
2025-12-27 13:36:01 +0300

    added AttachmentDto

---
+++ SharedDTOs/AttachmentDto.cs
@@ -0,0 +1,14 @@
+﻿namespace SharedDTOs
+{
+    public class AttachmentDto
+    {
+        public Guid Id { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public DateTime CreateDate { get; set; }
+        public Guid UploadedBy { get; set; }
+        public string UploadedByFullName { get; set; }
+        public Guid TicketId { get; set; }
+        public Guid? CommentId { get; set; }
+    }
+}
