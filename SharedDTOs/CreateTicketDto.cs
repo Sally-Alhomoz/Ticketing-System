1b99747977448621f9494563e7fc645472350338
smara <smara@LAPTOP-1E2M6GME>
2025-12-29 00:01:23 +0300

    added Create ticket dto

---
+++ SharedDTOs/CreateTicketDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SharedDTOs
+{
+    public class CreateTicketDto
+    {
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public int productId { get; set; }
+    }
+}
