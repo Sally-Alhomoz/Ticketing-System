071fa7baa2e958f117557844261379c241c47bda
smara <smara@LAPTOP-1E2M6GME>
2025-12-29 22:39:32 +0300

    added FileUrl property

--- SharedDTOs/AttachmentDto.cs
+++ SharedDTOs/AttachmentDto.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; set; }
         public string FileName { get; set; }
         public string FilePath { get; set; }
+        public string FileUrl { get; set; }
         public DateTime CreateDate { get; set; }
         public Guid UploadedBy { get; set; }
         public string UploadedByFullName { get; set; }
