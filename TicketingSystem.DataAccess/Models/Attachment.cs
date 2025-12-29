d9e24264deaf7dec4d8e0bf6fd6ae7c46eddfa0a
smara <smara@LAPTOP-1E2M6GME>
2025-12-29 22:35:02 +0300

    add foreign key relationships to model

--- TicketingSystem.DataAccess/Models/Attachment.cs
+++ TicketingSystem.DataAccess/Models/Attachment.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TicketingSystem.DataAccess.Models
 {
@@ -10,8 +11,11 @@ namespace TicketingSystem.DataAccess.Models
         public string FilePath { get; set; }
         public DateTime CreateDate { get; set; }
         public Guid UploadedBy { get; set; }
+        [ForeignKey("UploadedBy")]
         public User UploadedByUser { get; set; }
         public Guid TicketId { get; set; }
+        [ForeignKey("TicketId")]
+        public Ticket Ticket { get; set; }
         public Guid? CommentId { get; set; }
     }
 }
--- TicketingSystem.DataAccess/Models/Ticket.cs
+++ TicketingSystem.DataAccess/Models/Ticket.cs
@@ -1,5 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
-using SharedDTOs.Enum;
+﻿using SharedDTOs.Enum;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TicketingSystem.DataAccess.Models
 {
@@ -13,10 +14,13 @@ namespace TicketingSystem.DataAccess.Models
         public TicketPriority Priority { get; set; }
         public DateTime CreateDate { get; set; }
         public int productId { get; set; }
+        [ForeignKey("productId")]
         public Product product { get; set; }
         public Guid CreatedBy { get; set; }
+        [ForeignKey("CreatedBy")]
         public virtual User Creator { get; set; }
         public Guid? AssignedTo { get; set; }
+        [ForeignKey("AssignedTo")]
         public virtual User AssignedUser { get; set; }
     }
 }
