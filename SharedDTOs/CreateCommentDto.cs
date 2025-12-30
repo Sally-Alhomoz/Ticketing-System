0e6524eb2b301130be1b1c18418fd84f83452b33
smara <smara@LAPTOP-1E2M6GME>
2025-12-30 12:15:51 +0300

    added Create Comment Dto

---
+++ SharedDTOs/CreateCommentDto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace SharedDTOs
+{
+    public class CreateCommentDto
+    {
+        public string Message { get; set; }
+        public Guid TicketId { get; set; }
+        public List<IFormFile>? Files { get; set; }
+    }
+}
