5c39e8c5e0c6b2bc4a2ee18c5183f52b51be16ae
smara <smara@LAPTOP-1E2M6GME>
2025-12-30 12:24:59 +0300

    Added SaveFileAsync to handle physical storage

--- TicketingSystem.Services/Interfaces/IAttachmentService.cs
+++ TicketingSystem.Services/Interfaces/IAttachmentService.cs
@@ -1,11 +1,13 @@
-﻿using SharedDTOs;
+﻿using Microsoft.AspNetCore.Http;
+using SharedDTOs;
 
 namespace TicketingSystem.Services.Interfaces
 {
     public interface IAttachmentService
     {
-        Task AddAttachment(AttachmentDto dto);
+        Task AddAttachments(List<IFormFile> files, Guid userId, Guid ticketId, Guid? commentId = null);
         Task<AttachmentDto?> GetAttachmentById(Guid id);
         Task<List<AttachmentDto>> GetByTicketId(Guid ticketId);
+        Task<List<AttachmentDto>> GetByCommentId(Guid commentId);
     }
 }
--- TicketingSystem.Services/Services/AttachmentService.cs
+++ TicketingSystem.Services/Services/AttachmentService.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.Extensions.Logging;
-using TicketingSystem.DataAccess.UnitOfWork;
-using TicketingSystem.Services.Interfaces;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using SharedDTOs;
 using TicketingSystem.DataAccess.Models;
-using Microsoft.EntityFrameworkCore;
+using TicketingSystem.DataAccess.UnitOfWork;
+using TicketingSystem.Services.Interfaces;
+using Attachment = TicketingSystem.DataAccess.Models.Attachment;
 
 namespace TicketingSystem.Services.Services
 {
@@ -17,33 +18,51 @@ namespace TicketingSystem.Services.Services
             _logger = logger;
         }
 
-        public async Task AddAttachment(AttachmentDto dto)
+        private async Task<string> SaveFileAsync(IFormFile file)
         {
-            _logger.LogInformation("Adding an attachment.");
+            var rootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
+            if (!Directory.Exists(rootPath)) Directory.CreateDirectory(rootPath);
 
-            var attachment = new Attachment
+            var uniqueFileName = $"{Guid.NewGuid()}_{file.FileName}";
+            var fullPath = Path.Combine(rootPath, uniqueFileName);
+
+            using (var stream = new FileStream(fullPath, FileMode.Create))
             {
-                Id = Guid.NewGuid(),
-                UploadedBy = dto.UploadedBy,
-                FileName = dto.FileName,
-                FilePath = dto.FilePath,
-                CreateDate=DateTime.UtcNow,
-                TicketId = dto.TicketId,
-                CommentId = dto.CommentId
-            };
+                await file.CopyToAsync(stream);
+            }
 
-            _uow.Attachments.Add(attachment);
-            await _uow.Complete();
-            _logger.LogInformation("Attachment added successfully.");
+            return fullPath;
         }
+
+        public async Task AddAttachments(List<IFormFile> files, Guid userId, Guid ticketId, Guid? commentId = null)
+        {
+            foreach (var file in files)
+            {
+                var physicalPath = await SaveFileAsync(file);
+
+                var attachment = new Attachment
+                {
+                    Id = Guid.NewGuid(),
+                    FileName = file.FileName,
+                    FilePath = physicalPath,
+                    CreateDate = DateTime.Now,
+                    UploadedBy = userId,
+                    TicketId = ticketId,
+                    CommentId = commentId 
+                };
+
+                _uow.Attachments.Add(attachment);
+            }
+        }
+
 
         public async Task<AttachmentDto?> GetAttachmentById(Guid id)
         {
             _logger.LogInformation("Retrieving attachemnt with id :{Id}", id);
 
-            var attachmen = await _uow.Attachments.GetAttachmentById(id);
+            var attachment = await _uow.Attachments.GetAttachmentById(id);
 
-            if(attachmen == null)
+            if(attachment == null)
             {
                 _logger.LogWarning("No attachment found.");
                 return null;
@@ -51,14 +70,15 @@ namespace TicketingSystem.Services.Services
 
             var dto = new AttachmentDto
             {
-                Id = attachmen.Id,
-                CreateDate = attachmen.CreateDate,
-                FileName = attachmen.FileName,
-                FilePath = attachmen.FilePath,
-                CommentId = attachmen.CommentId,
-                TicketId = attachmen.TicketId,
-                UploadedBy = attachmen.UploadedBy,
-                UploadedByFullName = attachmen.UploadedByUser.FirstName + " " + attachmen.UploadedByUser.LastName
+                Id = attachment.Id,
+                CreateDate = attachment.CreateDate,
+                FilePath=attachment.FilePath,
+                FileName = attachment.FileName,
+                FileUrl = $"/api/Attachments/Download/{attachment.Id}",
+                CommentId = attachment.CommentId,
+                TicketId = attachment.TicketId,
+                UploadedBy = attachment.UploadedBy,
+                UploadedByFullName = attachment.UploadedByUser.FirstName + " " + attachment.UploadedByUser.LastName
             };
 
             _logger.LogInformation("Attachment retrieved successfully.");
@@ -67,7 +87,7 @@ namespace TicketingSystem.Services.Services
 
         public async Task<List<AttachmentDto>> GetByTicketId(Guid ticketId)
         {
-            _logger.LogInformation("Retrieving attachemnts for user {TicketId}", ticketId);
+            _logger.LogInformation("Retrieving attachemnts for ticket {TicketId}", ticketId);
 
             var attachmens = _uow.Attachments.GetByTicketId(ticketId).OrderBy(a => a.CreateDate);
 
@@ -75,7 +95,8 @@ namespace TicketingSystem.Services.Services
             {
                 Id = a.Id,
                 FileName = a.FileName,
-                FilePath = a.FilePath,
+                FilePath=a.FilePath,
+                FileUrl = $"/api/Attachments/Download/{a.Id}",
                 TicketId = a.TicketId,
                 CreateDate=a.CreateDate,
                 UploadedBy = a.UploadedBy,
@@ -85,7 +106,34 @@ namespace TicketingSystem.Services.Services
 
             if(dto.Count == 0)
             {
-                _logger.LogWarning("No comments found for Ticket");
+                _logger.LogWarning("No attachments found for Ticket");
+            }
+
+            return dto;
+        }
+
+        public async Task<List<AttachmentDto>> GetByCommentId(Guid commentId)
+        {
+            _logger.LogInformation("Retrieving attachemnts for user ");
+
+            var attachmens = _uow.Attachments.GetByCommentId(commentId).OrderBy(a => a.CreateDate);
+
+            var dto = attachmens.Select(a => new AttachmentDto
+            {
+                Id = a.Id,
+                FileName = a.FileName,
+                FilePath = a.FilePath,
+                FileUrl = $"/api/Attachments/Download/{a.Id}",
+                TicketId = a.TicketId,
+                CreateDate = a.CreateDate,
+                UploadedBy = a.UploadedBy,
+                UploadedByFullName = a.UploadedByUser.FirstName + " " + a.UploadedByUser.LastName,
+                CommentId = a.CommentId
+            }).ToList();
+
+            if (dto.Count == 0)
+            {
+                _logger.LogWarning("No attachments found for this comment.");
             }
 
             return dto;
