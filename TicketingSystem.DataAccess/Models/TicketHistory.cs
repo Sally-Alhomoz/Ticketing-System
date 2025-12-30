36f1654226a310495e7c013594718020d835e2db
smara <smara@LAPTOP-1E2M6GME>
2025-12-30 12:17:35 +0300

    added Foreign Key relations

--- TicketingSystem.DataAccess/Models/TicketHistory.cs
+++ TicketingSystem.DataAccess/Models/TicketHistory.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using SharedDTOs.Enum;
 
 namespace TicketingSystem.DataAccess.Models
@@ -10,8 +11,10 @@ namespace TicketingSystem.DataAccess.Models
         public DateTime ChangeDate { get; set; }
         public TicketStatus NewStatus { get; set; }
         public Guid TicketId { get; set; }
+        [ForeignKey("TicketId")]
         public Ticket Ticket { get; set; }
         public Guid ChangedBy { get; set; }
+        [ForeignKey("ChangedBy")]
         public User ChangedByUser { get; set; }
     }
 }
